0d81a48a5b12d34820dc63dc652c974578b368df
mnichol3 <nicholson.matt94@gmail.com>
2019-02-04 14:05:47 -0500

    Created manager.py

---
+++ src/manager.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  4 14:04:02 2019
+
+@author: Salty Pete
+
+This file will contain functions that will organize the date times, 
+gather the glm, abi, & acft data, and make the necessary function calls to 
+produce the output
+"""
+
