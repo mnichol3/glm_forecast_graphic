21be6ef911d01349833b196ac12c438870c51e09
mnichol3 <nicholson.matt94@gmail.com>
2019-02-13 12:11:52 -0500

    Modified documentation

--- src/aws_bucket.py
+++ src/aws_bucket.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Oct 23 15:05:28 2018
 
-@author: Salty Pete
+@author: Matt Nicholson
 
 Adapted from Alex Chan's code found here
 -> https://alexwlchan.net/2017/07/listing-s3-keys/
@@ -18,95 +18,106 @@ import sys
 
 
 
-###############################################################################
-# Calculates the julian day (days since 01 Jan) from the date param. Needed to 
-# download from AWS server
-#
-# @param    date (str)          Date to convert to julian day. Format: YYYYMMDD
-#                               HH and/or MM (minute) may be included at the end but not
-#                               required (e.g., YYYYMMDDHHMM)
-#
-# @return   julian_day (str)    Converted julian day date. 
-###############################################################################
+
 def calc_julian_day(date):
+    """
+    Calculates the Julian Day (days since 01 Jan) for a given date. Needed to
+    download files from AWS server
+
+    Parameters
+    ------------
+    date : str
+        Date to be converted to julian day. Format: YYYYMMDD, YYYYMMDDHH, or
+        YYYYMMDDHHMM
+
+    Returns
+    ------------
+    julian_day : str
+        Date converted to Julian Day
+    """
     # Format: YYYYMMDDHHMM
-    
+
                     # J   F   M   A   M   J   J   A   S   O   N   D
                     # 1   2   3   4   5   6   7   8   9   10  11  12
                     # 0   1   2   3   4   5   6   7   8   9   10  11
     days_per_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    
+
     leap_years = [2016, 2020, 2024, 2028, 2032]
-    
+
     year = date[:4]
     month = date[4:6]
     day = date[6:8]
-    
+
     if (int(year) in leap_years):
         days_per_month[1] += 1
-    
+
     curr_month = int(month) - 2
     julian_day = int(day)
-    
+
     while (curr_month >= 0):
         julian_day += days_per_month[curr_month]
         curr_month -= 1
-    
+
     julian_day = str(julian_day)
-    
+
     if (int(julian_day) < 100):
         julian_day = '0' + julian_day
-        
+
         if (int(julian_day) < 10):
             julian_day = '0' + julian_day
-            
+
     return julian_day
 
 
 
-###############################################################################
-# Downloads GOES-16 GLM data files from NOAA's AWS server
-#
-# @param    date (str)                  Date & time of the desired files. Time 
-#                                       is 1-hr block. Format: YYYYMMDDHH
-#
-# @return   glm_fnames (list of str)    List of filenames downloaded. 
-#                                       Format: YYYYMMDDHHMM.nc
-###############################################################################
 def glm_dl(date):
+    """
+    Download GOES-16 GLM data files from NOAA's Amazon AWS server
+
+    Parameters
+    ------------
+    date : str
+        Date & time of the desired files, in a 1-hr block. Format: YYYYMMDDHH
+
+    Returns
+    ------------
+    glm_fnames : list of str
+        List of downloaded GLM filenames as they are stored locally
+        Format: YYYMMDDHHMM.nc
+    """
     glm_fnames = []
-    
+
     s3 = boto3.client('s3')
-    s3.meta.events.register('choose-signer.s3.*', disable_signing) 
-    
+    s3.meta.events.register('choose-signer.s3.*', disable_signing)
+
     year = date[:4]
     hour = date[-2:]
     julian_day = calc_julian_day(date)
-    
+
     keys = []
     prefix = 'GLM-L2-LCFA/' + year + '/' + julian_day + '/' + hour + '/OR_GLM-L2-LCFA_G16'
     suffix = ''
     kwargs = {'Bucket': 'noaa-goes16', 'Prefix': prefix}
-    
+
     while True:
         resp = s3.list_objects_v2(**kwargs)
         for obj in resp['Contents']:
             key = obj['Key']
             if key.endswith(suffix):
                 keys.append(key)
-    
+
         try:
             kwargs['ContinuationToken'] = resp['NextContinuationToken']
         except KeyError:
             break
-    
+
     path = 'D:\Documents\senior-research-data\glm'
     dl_count = 0
-    
+
     for x in keys:
-       
+
         fname_match = re.search('s(\d{14})', x)
-        
+
         if (fname_match):
             fname = fname_match[1] + '.nc'
             try:
@@ -120,36 +131,42 @@ def glm_dl(date):
                     print("The file does not exist in this AWS bucket.")
                 else:
                     print("Error downloading file from AWS")
-    
-    sys.stdout.write("\rFinished! Files downloaded: {}              ".format(dl_count))   
+
+    sys.stdout.write("\rFinished! Files downloaded: {}              ".format(dl_count))
     sys.stdout.flush()
-    
+
     return glm_fnames
 
 
 
-###############################################################################
-# Downloads GOES-16 ABI data files from NOAA's AWS server
-#
-# @param    date (str)                  Date & time of the desired files. Time 
-#                                       is 1-hr block. Format: YYYYMMDDHH
-#
-# @param    sector (str)                M1 -> meso 1, M2 -> meso 2, C -> conus
-#
-# @return   abi_fnames (list of str)    List of filenames downloaded. 
-#                                       Format: YYYYMMDDHHMM.nc
-###############################################################################
 def abi_dl(date, sector):
-    
+    """
+    Downlad GOES-16 ABI data files from NOAA's Amazon AWS server
+
+    Parameters
+    ------------
+    date : str
+        Date & time of the desired files, in a 1-hr block. Format: YYYYMMDDHH
+    sector : str
+        Sector of the GOES-16 imagery to download. "M1" -> mesoscale 1,
+        "M2" -> mesoscale 2, "C" -> CONUS
+
+    Returns
+    ------------
+    abi_fnames : list of str
+        List of filenames downloaded as they are stored locally.
+        Format: YYYYMMDDHHMM.nc
+    """
+
     abi_fnames = []
-    
+
     s3 = boto3.client('s3')
-    s3.meta.events.register('choose-signer.s3.*', disable_signing) 
-    
+    s3.meta.events.register('choose-signer.s3.*', disable_signing)
+
     keys = []
     # RadM1 for Meso sector 1
     # RadM2 for Meso sector 2
-    
+
     if (sector == 'meso1'):
         sector_prefix = 'M1'
     elif (sector == 'meso2'):
@@ -159,34 +176,34 @@ def abi_dl(date, sector):
     else:
         print('Error: Invalid sector parameter!')
         return
-    
+
     year = date[:4]
     hour = date[-2:]
     julian_day = calc_julian_day(date)
-    
+
     prefix = 'ABI-L1b-RadM/' + year + '/' + julian_day + '/' + hour + '/OR_ABI-L1b-Rad' + sector_prefix + '-M3C01_G16'
     suffix = ''
     kwargs = {'Bucket': 'noaa-goes16', 'Prefix': prefix}
-    
+
     while True:
         resp = s3.list_objects_v2(**kwargs)
         for obj in resp['Contents']:
             key = obj['Key']
             if key.endswith(suffix):
                 keys.append(key)
-    
+
         try:
             kwargs['ContinuationToken'] = resp['NextContinuationToken']
         except KeyError:
             break
-    
+
     path = 'D:\Documents\senior-research-data\abi'
     dl_count = 0
-    
+
     for x in keys:
-        
+
         fname_match = re.search('s(\d{11})', x)
-        
+
         if (fname_match):
             fname = fname_match[1] + '.nc'
             try:
@@ -200,8 +217,8 @@ def abi_dl(date, sector):
                     print("The file does not exist in this AWS bucket.")
                 else:
                     print("Error downloading file from AWS")
-    
-    sys.stdout.write("\rFinished! Files downloaded: {}              ".format(dl_count))   
+
+    sys.stdout.write("\rFinished! Files downloaded: {}              ".format(dl_count))
     sys.stdout.flush()
-    
+
     return abi_fnames
--- src/parallax_error.py
+++ src/parallax_error.py
@@ -2,89 +2,160 @@
 """
 Created on Thu Feb  7 14:31:55 2019
 
-@author: Salty Pete
+@author: Matt Nicholson
 """
 
 import math
 
+R = 6378    # Earth radius
+H = 35786   # Satellite height above Earth surface
 
 
-# Returns tan^2(x) based on the trig identity:
-#
-#     1 - cos(2x)
-#    ------------- = tan^2(x)
-#     1 + cos(2x)
-#
+
 def tan_sqr(deg):
+    """
+    Returns tan^2(x), based on the trig identity:
+
+    1 - cos(2x)
+    ------------- = tan^2(x)
+    1 + cos(2x)
+
+    Parameters
+    ------------
+    deg : int or float
+        Angle, in degrees, to plug into tan^2(x)
+
+    Returns
+    ------------
+    result : int/float
+        tan^2(deg)
+    """
     rad = math.radians(deg)
     num = 1 - math.cos(2 * rad)
     denom = 1 + math.cos(2 * rad)
     result = num / denom
-    
+
     return result
 
 
 
-# 4.4
-# cos(ups) > 1/P
 def calc_viz_lim(P):
+    """
+    Eq. 4.4
+    Calculates the visibility limit of the satellite, which is dependent on
+    the satellite's altitude.
+
+    Parameters
+    ------------
+    P : float
+        The projection parameter of the satellite
+
+    Returns
+    ------------
+    ups : float
+        The visibility limit of the satellite
+    """
     rhs = 1/P
     ups = math.acos(math.radians(rhs))
     return ups
-    
-    
-    
-    
-# 4.1
-# Projection parameter
-# Ratio between the distance to sphere center and sphere radius
+
+
+
 def calc_proj_param(R, H):
+    """
+    Eq. 4.1
+    Calculates the projection parameter of a geostationary satellite, which is
+    the ratio between the distance to the earth's center and the
+    earth's radius
+
+    Parameters
+    ------------
+    R : int
+        Radius of the the Earth, in km
+    H : int
+        Satellite's height above the Earth's surface, in km
+
+    Returns
+    ------------
+    P : float
+        Geostationary satellite's projection paramter
+    """
     P = (R + H) / R
     return P
 
 
 
-# 4.2
-# a = nadir angle
-# ups = angular distance from SSP
-def calc_nadir_angle(P, ups):
-    num = math.sin(math.radians(ups))
-    denom = P - math.cos(math.radians(ups))
-    tan_a = num / denom
-    a = math.atan(math.radians(tan_a))
-    return math.radians(a)
-
-
-
-# 4.3
-def calc_nadir_angle_inv(P, a):
-    fp = P - math.sqrt(1 - (P**2 - 1) * tan_sqr(a))
-    sp = math.tan(math.radians(a)) / (1 + tan_sqr(a))
-    
-    sin_ups = fp + sp
-    ups = math.asin(math.radians(sin_ups))
-    return ups
-    
-
-
-# 4.5
 def calc_ssp_dist(lon, lat):
+    """
+    Eq. 4.5
+    Calculates the geostationary satellite's Satellite Sub Point (SSP) distance
+    ange (ups) for a given longitude & latitude
+
+    Parameters
+    ------------
+    lon : float
+        Longitude coordinate, in decimal degrees
+    lat : float
+        Latitude coordinate, in decimal degrees
+
+    Returns
+    ------------
+    ups: float
+        The SSP distance angle for the given longitude & latitude coordinates,
+        in degrees
+    """
     cos_ups = math.cos(math.radians(lon)) + math.cos(math.radians(lat))
     ups = math.acos(math.radians(cos_ups))
     return math.degrees(ups)
 
 
 
-# 4.11
-# lil_h = altitude of elevated cloud layers
 def corrected_proj_param(big_H, lil_h, R):
+    """
+    Eq. 4.11
+    Calculates the corrected projection parameter for a cloud target
+
+    Parameters
+    ------------
+    big_H : int
+        Satellite's height above the Earth's surface, in km
+    lil_h : int
+        Cloud's altitude above the Earth's surface, in km
+    R : int
+        Earth's radius, in km
+
+    Returns
+    ------------
+    P_h : float
+        Corrected projection parameter
+    """
     P_h = (R + big_H) / (R + lil_h)
     return P_h
-    
-    
 
-# 4.12
+
+
 def lin_parallaxE(ups, lil_h, R, P):
+    """
+    Eq. 4.12
+    Calculates the linearized expression for parallax error as a function of
+    the SSP distance angle
+
+    Parameters
+    ------------
+    ups : float
+        The SSP distance angle, in degrees
+    lil_h : int/float
+        The cloud's altitude above the Earth's surface, in km
+    R : int
+        Earth's radius, in km
+    P : float
+        Satellite projection parameter
+
+    Returns
+    ------------
+    delta_ups : float
+        Angular parallex error, in degrees
+    """
     num = P * math.sin(math.radians(ups))
     denom = P * math.cos(math.radians(ups)) - 1
     delta_ups = (num / denom) * (lil_h / R)
@@ -92,51 +163,118 @@ def lin_parallaxE(ups, lil_h, R, P):
 
 
 
-# 4.13
-# Parallax error as a function of longitude & latitude
 def parallaxE_lon_lat(lon, lat, lil_h, P, R):
+    """
+    Eq. 13
+    Calculates satellite parallax error as a function of the longitude & latitude
+    of an elevated target
+
+    Parameters
+    ------------
+    lon : float
+        Longitude coordinate of the elevated target, in decimal degrees
+    lat : float
+        Latitude coordinate of the elevated target, in decimal degrees
+    lil_h : int/float
+        Altitude of the elevated target above the Earth's surface
+    P : float
+        Satellite's projection parameter
+    R : int
+        Earth's radius, in km
+
+    Returns
+    ------------
+    delta_ups : float
+        Angular parallax error, in degrees
+    """
     rad_lon = math.radians(lon)
     rad_lat = math.radians(lat)
-    
+
     num = math.cos(rad_lon)**2
     num  = num * ((math.cos(rad_lat)**2))
     num = P * math.sqrt(1 - num)
-    
+
     denom = P * (math.cos(math.radians(lon))) * (math.cos(math.radians(lat))) - 1
-    
+
     delta_ups = (num / denom) * (lil_h / R)
-    
+
     return delta_ups
 
 
 
-# 4.14
-# Longitude & Latitude parallax error linear approximation
 def lin_parallaxE_lon_lat(delta_ups, ups, lon, lat):
+    """
+    Eq. 4.14
+    Linear approximation of the parallax error as a function of the elevated
+    target's longitude and latitude
+
+    Parameters
+    ------------
+    delta_ups : float
+        Parallax error, in km
+    ups : float
+        SSP distance angle, in degrees
+    lon : float
+        Elevated target's longitude coordinate, in degrees
+    lat : float
+        Elevated target's latitude coordinate, in degrees
+
+    Returns
+    ------------
+    tuple containing 2 floats
+    [0] = the longitude parallax error
+    [1] = the latitude parallax error
+    """
     delta_lon = lon * (delta_ups / ups)
     delta_lat = lat * (delta_ups / ups)
-    
+
     return (delta_lon, delta_lat)
 
 
 
 def parallaxE_dist(delta_ups, R):
-    return (delta_ups * R)    
+    """
+    Calculates the distance value of the parallax error in km
 
- 
-    
-# Since GOES-16 central longitude is -75.2 deg and not 0
+    Parameters
+    ------------
+    delta_ups : float
+        Angular parallax error, in degrees
+    R : int
+        Earth's radius, in km
+
+    Returns
+    ------------
+    delta_ups * R : float
+        Parallax error as a distance, in km
+
+    """
+    return (delta_ups * R)
+
+
+
 def calc_lon(lon):
+    """
+    Calculates the latitude of an elevated target relative to the GOES-16
+    central longitude of -75.2 (75.2 deg W)
+
+    Parameters
+    ------------
+    lon : float
+        Longitude of an elevated target
+
+    Returns
+    ------------
+    lon + 75.2 : float
+        Longitude of the elevated target relative to GOES-16
+    """
     return lon + 75.2
 
 
 
-R = 6378    # Earth radius
-H = 35786   # Satellite height above Earth surface
-
 cent_lat = 29.93
 cent_lon = -71.35
-    
+
 P = calc_proj_param(R, H) # = 6.61
 ups = calc_ssp_dist(cent_lon, cent_lat)
 #delta_ups = lin_parallaxE(30, 12, R, P)
