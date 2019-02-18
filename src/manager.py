cf2e21742000d909798dc43e973940a9a4010cad
mnichol3 <nicholson.matt94@gmail.com>
2019-02-18 10:53:08 -0500

    Added get_os() func

--- src/manager.py
+++ src/manager.py
@@ -12,6 +12,28 @@ produce the output
 import aws_dl
 import GLM_plotter
 
+
+
+def get_os():
+    """
+    Determines the type of operating system being used. Needed for when we are
+    loading & saving local files later
+
+    Parameters
+    ------------
+    none
+
+    Returns
+    ------------
+    os_type : str
+        Type of OS the script is running on
+        Ex: 'linux' is the script is running on a Linux OS, such as Ubuntu
+    """
+    os_type = sys.platform
+    return os_type
+
+
+
 def main():
     #print(aws_dl.get_os() == 'linux')
     #print(aws_dl.date_time_chunk('2018091218', '2018091305'))
