53690141ee90cbcbcb394cea0c7f84c3ceede483
mnichol3 <nicholson.matt94@gmail.com>
2019-03-31 18:08:06 -0400

    Created config file

---
+++ src/config.py
@@ -0,0 +1,16 @@
+"""
+Created on Sun Mar 31
+
+@author: Matt Nicholson
+"""
+
+def init():
+    global PATH_LINUX
+    global PATH_LINUX_ABI
+    global PATH_LINUX_GLM
+    global PATH_LINUX_VDM
+
+    PATH_LINUX = '/media/mnichol3/easystore/data'
+    PATH_LINUX_ABI = '/media/mnichol3/easystore/data/abi'
+    PATH_LINUX_GLM = '/media/mnichol3/easystore/data/glm'
+    PATH_LINUX_VDM = '/media/mnichol3/easystore/data/vdm'
