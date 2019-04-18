693a95c7142c317f9633d1165ab7cff39416585f
mnichol3 <nicholson.matt94@gmail.com>
2019-04-18 17:01:15 -0400

    Added function to read local SHIPS files

--- src/ships_parse.py
+++ src/ships_parse.py
@@ -28,12 +28,127 @@ SHIPS filename format:
 import urllib.request
 import sys
 import pandas as pd
-from os.path import join, exists
-from os import makedirs
+from os.path import join, exists, isfile
+from os import makedirs, listdir
 import csv
+
+
 
 BASE_URL = 'ftp://ftp.nhc.noaa.gov/atcf/stext'
 PATH_LINUX_SHIPS = '/media/mnichol3/easystore/data/SHIPS'
+
+
+
+def fetch_file_local(datetime, storm_name, basin='AL', write=False):
+    """
+    Locates and parses a SHIPS file for the given datetime & storm
+
+    Parameters
+    ----------
+    datetime : str
+        Date & time of the desired SHIPS data. Format: YYYYMoMoDDHHMM
+    storm_name : str
+        Name of the storm the desired SHIPS file is related to
+    basin : str
+        Basin in which the storm is located. AL = Atlantic, EP = Eastern Pacific
+
+
+    Returns
+    -------
+    data_dict : dictionary of type key : str, value : str
+        Dictionary containing relevant SHIPS data. Keys: datetime, shear_spd,
+        shear_dir, sst
+
+    """
+    html = None
+    data = None
+    hour_diff = 0
+    data_dict = {}
+    synop_times = ['00', '06', '12', '18']
+    storm_nums = {'FLORENCE': '06', 'MICHAEL': '14'}
+    base_path = PATH_LINUX_SHIPS + "/ftp"
+
+    storm_name = storm_name.upper()
+    if (storm_name not in list(storm_nums.keys())):
+        print('Error: Storm name not in storm number dictionary (ships_parse.fetch_files)')
+        sys.exit(0)
+
+    fnames = [f for f in listdir(base_path) if isfile(join(base_path, f))]
+
+    if (fnames):
+
+        storm_name = storm_name.upper()
+
+        year = datetime[2:4]
+        month = datetime[4:6]
+        day = datetime[6:8]
+        hour_true = datetime[8:10]
+
+        # Determine the hour for the filename
+        if (hour_true not in synop_times):
+            if (hour_true < '18'):
+                prev_idx = next(idx for idx, value in enumerate(synop_times) if value > hour_true)
+                prev_idx -= 1
+                hour_adj = synop_times[prev_idx]
+            else:
+                hour_adj = '18'
+
+            hour_diff = int(hour_true) - int(hour_adj)
+            hour = hour_adj
+        else:
+            hour = hour_true
+
+        target_fname = year + month + day + hour + basin + storm_nums[storm_name] + year + '_ships.txt'
+
+        ftp_fname = join(base_path, target_fname)
+
+        print("Processing SHIPS file: " + ftp_fname + "\n")
+
+        with open(ftp_fname, 'r') as response:
+            data = response.read()
+
+        if (data):
+            data = data.splitlines()
+
+            idx = 2
+            while (idx < 14):
+                line = data[idx].split(' ')
+                line = [val for val in line if val != '']
+
+                if (idx == 2):
+                    if (storm_name in line):
+                        data_dict['datetime'] = datetime
+                    else:
+                        print("Error: Storm names do not match (ships_parse.fetch_files)")
+                        sys.exit(0)
+                elif (idx == 10):
+                    if (hour_diff == 0):
+                        data_dict['shear_spd'] = line[2]
+                    else:
+                        data_dict['shear_spd'] = line[3]
+                elif (idx == 12):
+                    if (hour_diff == 0):
+                        data_dict['shear_dir'] = line[2]
+                    else:
+                        data_dict['shear_dir'] = line[3]
+                elif (idx == 13):
+                    if (hour_diff == 0):
+                        data_dict['sst'] = line[2]
+                    else:
+                        data_dict['sst'] = line[3]
+                idx += 1
+        else:
+            print("Error retrieving SHIPS file data (ships_parse.fetch_files)")
+            sys.exit(0)
+
+
+    else:
+        print("Error retrieving SHIPS filenames (ships_parse.fetch_files)")
+        sys.exit(0)
+
+    return data_dict
+
+
 
 def fetch_file(datetime, storm_name, basin='AL', write=False):
     """
